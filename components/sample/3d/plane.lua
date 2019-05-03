30e8c02e52facdbc715f8e5e1ec9d9c714f162bf
notexplosive <thenotexplosive@gmail.com>
2019-05-02 21:45:20 -0700

    3D works??

--- components/sample/3d/plane.lua
+++ components/sample/3d/plane.lua
@@ -6,34 +6,48 @@ function Plane:awake()
 end
 
 function Plane:draw(x, y)
-    local depthScale = 0.75
+    local depthScale = 0.85
     -- Primary width is the width of the "Front"
-    local primaryWidth = 50
+    local primaryWidth = 64
     -- Secondary width is the width of the "Back"
-    local secondaryWidth = 50 * depthScale
+    local secondaryWidth = primaryWidth * depthScale
 
-    love.graphics.line(x - primaryWidth / 2, y*0.9, x + primaryWidth / 2, y*0.9)
-    love.graphics.line(x - secondaryWidth / 2, y * 2, x + secondaryWidth / 2, y * 2)
+    local distanceFromCenterX = love.graphics.getWidth()/2 - x
 
-    --[[
+    local horizontalScale = 0.1
+
+    local primaryY = (y - love.graphics.getHeight()/2)
+    local secondaryY = primaryY * depthScale
+
+    local points = {
+        x - primaryWidth / 2,
+        primaryY,
+        x + primaryWidth / 2,
+        primaryY,
+        x + secondaryWidth / 2 + distanceFromCenterX * horizontalScale,
+        secondaryY,
+        x - secondaryWidth / 2 + distanceFromCenterX * horizontalScale,
+        secondaryY
+    }
+
+    for i = 1, #points, 2 do
+        local px = points[i]
+        local py = points[i + 1]
+        points[i] = points[i] + primaryWidth / 2
+        points[i + 1] = points[i + 1] + love.graphics.getHeight()/2
+    end
+
+
+    if primaryY > secondaryY then
+        love.graphics.setColor(1,1,1)
+    else
+        love.graphics.setColor(1,0,1)
+    end
+
     love.graphics.polygon(
-        "line",
-        x - widthFront + 10 * horizAngle,
-        y - fov * vertAngle,
-        --
-        x + widthFront + 10 * horizAngle,
-        y - fov * vertAngle,
-        --
-        x - 100 + 10 * horizAngle,
-        y + fov * vertAngle,
-        --
-        x - 100 + 10 * horizAngle,
-        y + fov * vertAngle
+        "fill",
+        unpack(points)
     )
-    ]]
-end
-
-function Plane:update(dt)
 end
 
 return Plane
