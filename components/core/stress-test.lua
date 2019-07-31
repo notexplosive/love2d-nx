9e76e8c084ee43a47b88506c7db637bc86b7216d
notexplosive <thenotexplosive@gmail.com>
2019-07-30 21:45:17 -0700

    format

--- components/core/stress-test.lua
+++ components/core/stress-test.lua
@@ -1,18 +1,18 @@
 local StressTest = {}
 
-registerComponent(StressTest,'StressTest')
+registerComponent(StressTest, "StressTest")
 
 function StressTest:awake()
-    for i=1, 3000 do
+    for i = 1, 3000 do
         local actor = self.actor:scene():addActor()
         actor:addComponent(Components.DestroyAtRandomTime)
         actor:addComponent(Components.MoveByRandom)
-        actor:addComponent(Components.SpriteRenderer, "linkin",'walk',2)
+        actor:addComponent(Components.SpriteRenderer, "linkin", "walk", 2)
     end
 end
 
 function StressTest:update(dt)
-    if 1/dt > 60 then
+    if 1 / dt > 60 then
         debugLog(#self.actor:scene():getAllActors())
         self.actor:destroy()
     end
