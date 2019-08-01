6e23aa0a726e000d522f648a183d5abe1afcbb5d
notexplosive <thenotexplosive@gmail.com>
2019-08-01 00:21:18 -0700

    Extracted from BoundingBoxEditor:draw()

--- components/core/bounding-box-editor.lua
+++ components/core/bounding-box-editor.lua
@@ -4,14 +4,14 @@ registerComponent(BoundingBoxEditor, "BoundingBoxEditor", {"BoundingBox"})
 
 function BoundingBoxEditor:awake()
     self.sideGrabHandleRects = {}
+    self.cornerGrabHandleRects = {}
     self.selectedIndex = nil
     self.grabHandleWidth = 10
 end
 
-function BoundingBoxEditor:draw(x, y)
+function BoundingBoxEditor:update(dt)
     local width = self.actor.BoundingBox:width()
     local height = self.actor.BoundingBox:height()
-    love.graphics.setColor(1, 1, 1, 1)
 
     self.sideGrabHandleRects = {
         self:getTopGrabHandleRect(),
@@ -20,11 +20,27 @@ function BoundingBoxEditor:draw(x, y)
         self:getRightGrabHandleRect()
     }
 
+    self.cornerGrabHandleRects = {
+        self:getTopLeftGrabHandleRect(),
+        self:getTopRightGrabHandleRect(),
+        self:getBottomRightGrabHandleRect(),
+        self:getBottomLeftGrabHandleRect()
+    }
+end
 
-
+function BoundingBoxEditor:draw(x, y)
+    love.graphics.setColor(1, 1, 1, 1)
     for i, rect in ipairs(self.sideGrabHandleRects) do
         local fill = "line"
         if self.selectedIndex == i then
+            fill = "fill"
+        end
+        love.graphics.rectangle(fill, rect:xywh())
+    end
+
+    for i, rect in ipairs(self.cornerGrabHandleRects) do
+        local fill = "line"
+        if self.selectedIndex == i + 4 then
             fill = "fill"
         end
         love.graphics.rectangle(fill, rect:xywh())
@@ -33,18 +49,18 @@ end
 
 function BoundingBoxEditor:onMouseMove(x, y, dx, dy)
     if self.selectedIndex then
-        if self.selectedIndex == 1 then
+        if self.selectedIndex == 1 or self.selectedIndex == 6 or self.selectedIndex == 5 then
             self.actor:setPos(self.actor:pos() + Vector.new(0, dy))
             self.actor.BoundingBox.size.height = self.actor.BoundingBox:height() - dy
         end
-        if self.selectedIndex == 2 then
+        if self.selectedIndex == 2 or self.selectedIndex == 7 or self.selectedIndex == 8 then
             self.actor.BoundingBox.size.height = self.actor.BoundingBox:height() + dy
         end
-        if self.selectedIndex == 3 then
+        if self.selectedIndex == 3 or self.selectedIndex == 5 or self.selectedIndex == 8 then
             self.actor:setPos(self.actor:pos() + Vector.new(dx, 0))
             self.actor.BoundingBox.size.width = self.actor.BoundingBox:width() - dx
         end
-        if self.selectedIndex == 4 then
+        if self.selectedIndex == 4 or self.selectedIndex == 6 or self.selectedIndex == 7 then
             self.actor.BoundingBox.size.width = self.actor.BoundingBox:width() + dx
         end
 
@@ -54,6 +70,16 @@ end
 
 function BoundingBoxEditor:onMousePress(x, y, button, wasRelease, isClickConsumed)
     if button == 1 and not wasRelease and not isClickConsumed then
+        if not self.actor.BoundingBox:getRect():isVectorWithin(x, y) then
+            for i, rect in ipairs(self.cornerGrabHandleRects) do
+                if rect:isVectorWithin(x, y) then
+                    self.selectedIndex = i + 4
+                    self.actor:scene():consumeClick()
+                    return
+                end
+            end
+        end
+
         for i, rect in ipairs(self.sideGrabHandleRects) do
             if rect:isVectorWithin(x, y) then
                 self.selectedIndex = i
@@ -68,6 +94,34 @@ function BoundingBoxEditor:onMousePress(x, y, button, wasRelease, isClickConsume
             self.actor:destroy()
         end
     end
+end
+
+function BoundingBoxEditor:getTopLeftGrabHandleRect()
+    local boundingRect = self.actor.BoundingBox:getRect()
+    local x = boundingRect:x() - self.grabHandleWidth
+    local y = boundingRect:y() - self.grabHandleWidth
+    return Rect.new(x, y, self.grabHandleWidth * 2, self.grabHandleWidth * 2)
+end
+
+function BoundingBoxEditor:getTopRightGrabHandleRect()
+    local boundingRect = self.actor.BoundingBox:getRect()
+    local x = boundingRect:x() - self.grabHandleWidth + boundingRect:width()
+    local y = boundingRect:y() - self.grabHandleWidth
+    return Rect.new(x, y, self.grabHandleWidth * 2, self.grabHandleWidth * 2)
+end
+
+function BoundingBoxEditor:getBottomRightGrabHandleRect()
+    local boundingRect = self.actor.BoundingBox:getRect()
+    local x = boundingRect:x() - self.grabHandleWidth + boundingRect:width()
+    local y = boundingRect:y() - self.grabHandleWidth + boundingRect:height()
+    return Rect.new(x, y, self.grabHandleWidth * 2, self.grabHandleWidth * 2)
+end
+
+function BoundingBoxEditor:getBottomLeftGrabHandleRect()
+    local boundingRect = self.actor.BoundingBox:getRect()
+    local x = boundingRect:x() - self.grabHandleWidth
+    local y = boundingRect:y() - self.grabHandleWidth + boundingRect:height()
+    return Rect.new(x, y, self.grabHandleWidth * 2, self.grabHandleWidth * 2)
 end
 
 function BoundingBoxEditor:getLeftGrabHandleRect()
