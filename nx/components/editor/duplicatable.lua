e1f674043d3a1a3e9e58a09022b0ea4170b80f2a
notexplosive <thenotexplosive@gmail.com>
2020-01-14 18:05:22 -0800

    Format file

--- nx/components/editor/duplicatable.lua
+++ nx/components/editor/duplicatable.lua
@@ -17,9 +17,12 @@ end
 function Duplicatable:Keybind_onPress(key)
     if key == self.keybind and self.actor.Selectable:selected() and self:canDuplicate() then
         local newActor = self.actor:scene():addActor()
-        newActor:setPos(self.actor:pos() + Vector.new(20,20))
+        newActor:setPos(self.actor:pos() + Vector.new(20, 20))
         newActor:setAngle(self.actor:angle())
-        newActor:addComponent(self.actor.EditorSerializable:getComponentClass(),self.actor.EditorSerializable:getArgs())
+        newActor:addComponent(
+            self.actor.EditorSerializable:getComponentClass(),
+            self.actor.EditorSerializable:getArgs()
+        )
         newActor:addComponent(Components.SelectWhenAble)
     end
 end
