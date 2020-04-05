4f48e0188e20e16997d5fa867e81fc78c35a94db
notexplosive <thenotexplosive@gmail.com>
2020-04-05 13:37:03 -0700

    REFACTOR - move from input.lua to scene-layers.lua

--- nx/input.lua
+++ nx/input.lua
@@ -2,41 +2,16 @@ local sceneLayers = require("nx/scene-layers")
 
 function love.mousepressed(x, y, button)
     love.mousemoved(x, y, 0, 0)
-    local isConsumed = false
-    for _, scene in sceneLayers:eachInReverseDrawOrder() do
-        scene.isClickConsumed = isConsumed
-        local mousePoint = Vector.new(x, y) / scene:getScale() + scene:getViewportPosition()
-        scene:onMousePress(mousePoint.x, mousePoint.y, button, false)
-        if scene.isClickConsumed then
-            isConsumed = true
-        end
-    end
+    sceneLayers:onMousePress(x, y, button)
 end
 
 function love.mousereleased(x, y, button)
     love.mousemoved(x, y, 0, 0)
-    local isConsumed = false
-    for _, scene in sceneLayers:eachInReverseDrawOrder() do
-        scene.isClickConsumed = isConsumed
-        local mousePoint = Vector.new(x, y) / scene:getScale() + scene:getViewportPosition()
-        scene:onMousePress(mousePoint.x, mousePoint.y, button, true)
-        if scene.isClickConsumed then
-            isConsumed = true
-        end
-    end
+    sceneLayers:onMouseRelease(x, y, button)
 end
 
 function love.mousemoved(x, y, dx, dy)
-    local isConsumed = false
-    for _, scene in sceneLayers:eachInReverseDrawOrder() do
-        scene.isHoverConsumed = isConsumed
-        local mousePoint = Vector.new(x, y) / scene:getScale() + scene:getViewportPosition()
-        local mouseDelta = Vector.new(dx, dy) / scene:getScale()
-        scene:onMouseMove(mousePoint.x, mousePoint.y, mouseDelta.x, mouseDelta.y)
-        if scene.isHoverConsumed then
-            isConsumed = true
-        end
-    end
+    sceneLayers:onMouseMove(x, y, dx, dy)
 end
 
 function love.keypressed(key, scancode)
--- nx/scene-layers.lua
+++ nx/scene-layers.lua
@@ -11,6 +11,43 @@ function sceneLayers:eachInReverseDrawOrder()
     return self:eachReversed()
 end
 
+function sceneLayers:onMousePress(x, y, button)
+    local isConsumed = false
+    for _, scene in self:eachInReverseDrawOrder() do
+        scene.isClickConsumed = isConsumed
+        local mousePoint = Vector.new(x, y) / scene:getScale() + scene:getViewportPosition()
+        scene:onMousePress(mousePoint.x, mousePoint.y, button, false)
+        if scene.isClickConsumed then
+            isConsumed = true
+        end
+    end
+end
+
+function sceneLayers:onMouseRelease(x, y, button)
+    local isConsumed = false
+    for _, scene in self:eachInReverseDrawOrder() do
+        scene.isClickConsumed = isConsumed
+        local mousePoint = Vector.new(x, y) / scene:getScale() + scene:getViewportPosition()
+        scene:onMousePress(mousePoint.x, mousePoint.y, button, true)
+        if scene.isClickConsumed then
+            isConsumed = true
+        end
+    end
+end
+
+function sceneLayers:onMouseMove(x, y, dx, dy)
+    local isConsumed = false
+    for _, scene in self:eachInReverseDrawOrder() do
+        scene.isHoverConsumed = isConsumed
+        local mousePoint = Vector.new(x, y) / scene:getScale() + scene:getViewportPosition()
+        local mouseDelta = Vector.new(dx, dy) / scene:getScale()
+        scene:onMouseMove(mousePoint.x, mousePoint.y, mouseDelta.x, mouseDelta.y)
+        if scene.isHoverConsumed then
+            isConsumed = true
+        end
+    end
+end
+
 -- Tests
 local Test = require("nx/test")
 Test.run(
