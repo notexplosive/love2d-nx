c25d6d573e9f11421c3d75602055b58937cac2eb
notexplosive <thenotexplosive@gmail.com>
2020-04-16 21:31:48 -0700

    NinePatch:setSprite

--- nx/components/core/nine-patch.lua
+++ nx/components/core/nine-patch.lua
@@ -3,36 +3,10 @@ local NinePatch = {}
 registerComponent(NinePatch, "NinePatch", {"BoundingBox"})
 
 function NinePatch:setup(spriteName, inflateWidth, inflateHeight, offsetX, offsetY)
-    self.spriteName = spriteName
-    self.sprite = Assets.images[spriteName]
-    assert(self.sprite, "no sprite with name " .. spriteName)
+    self:setSprite(spriteName)
+
     self.inflateSize = Size.new(inflateWidth or 0, inflateHeight or 0)
     self.offsetVector = Vector.new(offsetX or 0, offsetY or 0)
-
-    local gw, gh = self.sprite:getGridCellDimensions()
-
-    local top = 0
-    local left = 0
-    local right = gw * 2
-    local bottom = gh * 2
-    local middleX = gw
-    local middleY = gh
-
-    self.topBottomQuad = love.graphics.newQuad(0, 0, 0, 0, gw, gh)
-    self.leftRightQuad = love.graphics.newQuad(0, 0, 0, 0, gw, gh)
-    self.middleQuad = love.graphics.newQuad(0, 0, 0, 0, gw, gh)
-
-    self.imageAndQuads = {
-        top = {image = self:getCroppedImage(middleX, top), quad = self.topBottomQuad},
-        bottom = {image = self:getCroppedImage(middleX, bottom), quad = self.topBottomQuad},
-        left = {image = self:getCroppedImage(left, middleY), quad = self.leftRightQuad},
-        right = {image = self:getCroppedImage(right, middleY), quad = self.leftRightQuad},
-        middle = {image = self:getCroppedImage(middleX, middleY), quad = self.middleQuad},
-        topLeft = {image = self:getCroppedImage(left, top)},
-        topRight = {image = self:getCroppedImage(right, top)},
-        bottomLeft = {image = self:getCroppedImage(left, bottom)},
-        bottomRight = {image = self:getCroppedImage(right, bottom)}
-    }
 end
 
 function NinePatch:reverseSetup()
@@ -65,16 +39,53 @@ function NinePatch:draw(x, y)
     love.graphics.draw(self.imageAndQuads.bottomLeft.image, x, bottomY)
     love.graphics.draw(self.imageAndQuads.bottomRight.image, rightX, bottomY)
 
-    love.graphics.draw(self.imageAndQuads.top.image, self.imageAndQuads.top.quad, middleX, y)
-    love.graphics.draw(self.imageAndQuads.left.image, self.imageAndQuads.left.quad, x, middleY)
-    love.graphics.draw(self.imageAndQuads.right.image, self.imageAndQuads.right.quad, rightX, middleY)
-    love.graphics.draw(self.imageAndQuads.bottom.image, self.imageAndQuads.bottom.quad, middleX, bottomY)
-    love.graphics.draw(self.imageAndQuads.middle.image, self.imageAndQuads.middle.quad, middleX, middleY)
+    love.graphics.draw(self.imageAndQuads.top.image, self.topBottomQuad, middleX, y)
+    love.graphics.draw(self.imageAndQuads.left.image, self.leftRightQuad, x, middleY)
+    love.graphics.draw(self.imageAndQuads.right.image, self.leftRightQuad, rightX, middleY)
+    love.graphics.draw(self.imageAndQuads.bottom.image, self.topBottomQuad, middleX, bottomY)
+    love.graphics.draw(self.imageAndQuads.middle.image, self.middleQuad, middleX, middleY)
+end
+
+function NinePatch:getSpriteName()
+    return self.spriteName
+end
+
+function NinePatch:setSprite(spriteName)
+    self.spriteName = spriteName
+    self.sprite = Assets.images[spriteName]
+    assert(self.sprite, "no sprite with name " .. spriteName)
+
+    local gw, gh = self.sprite:getGridCellDimensions()
+
+    local top = 0
+    local left = 0
+    local right = gw * 2
+    local bottom = gh * 2
+    local middleX = gw
+    local middleY = gh
+
+    self.topBottomQuad = love.graphics.newQuad(0, 0, 0, 0, gw, gh)
+    self.leftRightQuad = love.graphics.newQuad(0, 0, 0, 0, gw, gh)
+    self.middleQuad = love.graphics.newQuad(0, 0, 0, 0, gw, gh)
+
+    self.imageAndQuads =
+        self.sprite.cachedNinePatchImagesQuads or
+        {
+            top = {image = self:getCroppedImage(middleX, top)},
+            bottom = {image = self:getCroppedImage(middleX, bottom)},
+            left = {image = self:getCroppedImage(left, middleY)},
+            right = {image = self:getCroppedImage(right, middleY)},
+            middle = {image = self:getCroppedImage(middleX, middleY)},
+            topLeft = {image = self:getCroppedImage(left, top)},
+            topRight = {image = self:getCroppedImage(right, top)},
+            bottomLeft = {image = self:getCroppedImage(left, bottom)},
+            bottomRight = {image = self:getCroppedImage(right, bottom)}
+        }
+
+    self.sprite.cachedNinePatchImagesQuads = self.sprite.cachedNinePatchImagesQuads or self.imageAndQuads
 end
 
 function NinePatch:getCroppedImage(x, y)
-    -- TODO: move this to Sprite so it can be cached there?
-    -- if we have multiple Ninepatches with the same texture we shouldn't do this calculation twice
     local width, height = self.sprite.gridWidth, self.sprite.gridHeight
     local img = love.image.newImageData(self.sprite.filename)
     local cropped = love.image.newImageData(width, height)
